4209642645e97e4204367a689132d03e1a40619d
martin-Dev <martin.dev45@gmail.com>
2025-01-08 14:50:16 +0100

    Thinking

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmorente <mmorente@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/08 14:24:23 by mmorente          #+#    #+#             */
+/*   Updated: 2025/01/08 14:31:22 by mmorente         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE.H
+# define GET_NEXT_LINE.H
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+#endif
+
+#include <stdlib.h>
+
+#endif
