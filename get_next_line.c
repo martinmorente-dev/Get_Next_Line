f47d380a4335dca9cef3977ef53381bd5273a543
martin-Dev <martin.dev45@gmail.com>
2025-01-13 20:16:51 +0100

    I still need to implemet the next_line function, del_buff function, and the read_function explained in the program

--- get_next_line.c
+++ get_next_line.c
@@ -3,16 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmorente <mmorente@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: mmorente <mmorente@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 14:20:13 by mmorente          #+#    #+#             */
-/*   Updated: 2025/01/13 13:00:52 by mmorente         ###   ########.fr       */
+/*   Updated: 2025/01/13 20:11:57 by mmorente         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *get_next_line(int fd)
+
+
+char	*get_next_line(int fd)
 {
-	char *line;
+	static char	*buffer;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd,0,0) < 0)
+		return (NULL);
+
+	buffer = 'a'; //read_function(int fd, char *res);
+	// check if this function fail (!buffer)
+	if (!buffer)
+		return (NULL);
+	line = 'z';//next_line(char *buffer)
+	buffer = 'b';// del_buf(char *buff) eliminate 
 }
