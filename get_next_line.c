9531174c4648e6fcdfa86c1333eb10702e79d0b4
martinmorente-Dev <martin.dev45@gmail.com>
2025-01-13 13:01:18 +0100

    Trying to do it

--- get_next_line.c
+++ get_next_line.c
@@ -3,32 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmorente <mmorente@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mmorente <mmorente@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 14:20:13 by mmorente          #+#    #+#             */
-/*   Updated: 2025/01/08 14:43:29 by mmorente         ###   ########.fr       */
+/*   Updated: 2025/01/13 13:00:52 by mmorente         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-
-char	*read_file(char *str)
+char *get_next_line(int fd)
 {
-	
-}
-
-char	*get_next_line(int fd)
-{
-	static char 	*buffer;
-	char			*line;
-
-	if (fd <= 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
-		return (NULL);
-	buffer = read(fd,BUFFER_SIZE);
-	if (!buffer)
-		return (NULL);
-	line = 'b';// Function read_file(buffer)
-	buffer = 'a';// Function delete_line(buffer)
-	return (line);
+	char *line;
 }
--- get_next_line.h
+++ get_next_line.h
@@ -3,20 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmorente <mmorente@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mmorente <mmorente@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 14:24:23 by mmorente          #+#    #+#             */
-/*   Updated: 2025/01/08 14:31:22 by mmorente         ###   ########.fr       */
+/*   Updated: 2025/01/13 12:19:03 by mmorente         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE.H
-# define GET_NEXT_LINE.H
+#define GET_NEXT_LINE .H
 
 #ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
+#define BUFFER_SIZE 42
 #endif
 
 #include <stdlib.h>
+
+char *get_next_line(int fd);
 
 #endif
