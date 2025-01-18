abe7e1df9575eee6572b785c2f93518a076645d2
Martin Morente Vargas <martin.dev45@gmail.com>
2025-01-18 12:32:13 +0100

    Update

--- get_next_line.c
+++ get_next_line.c
@@ -3,15 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmorente <mmorente@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mmorente <mmorente@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 14:20:13 by mmorente          #+#    #+#             */
-/*   Updated: 2025/01/13 20:11:57 by mmorente         ###   ########.fr       */
+/*   Updated: 2025/01/18 12:31:53 by mmorente         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+
+
+char	*read_function(int fd, char *res)
+{
+	char	*buffer;
+	int		byte_read;
+
+	if (!res)
+		return (NULL);
+	byte_read = 1;
+	while (byte_read > 0)
+	{
+		byte_read = read(fd, buffer, BUFFER_SIZE);
+		if (byte_read <= 0)
+			return (NULL);
+	}
+}
 
 
 char	*get_next_line(int fd)
@@ -21,8 +38,7 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd,0,0) < 0)
 		return (NULL);
-
-	buffer = 'a'; //read_function(int fd, char *res);
+	buffer = read_function(fd,buffer);
 	// check if this function fail (!buffer)
 	if (!buffer)
 		return (NULL);
